86a3c8e7b89d97baebbc93e77e648c9e25c104f3
ralphat <ralphat@users.noreply.github.com>
2015-05-20 13:40:21 +0530

    Update #15.cpp

--- #15.cpp
+++ #15.cpp
@@ -1,36 +1,12 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-using namespace std;
-long long ms(long long n)
-{
-    long long i,k=1;
-    for(i=n;i>1;i--){
-        k*=i;
-    }
-    return k;
-}
-long long comb(long long n, long long m)
-{
-    long long i,j,k,l;
-    i=ms(n+m);
-    j=ms(n);
-    k=ms(m);
-    l=j*k;
-    l=i/l;
-    return l%1000000007;
-}
- 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    long long a,n,m,i,j,k;
-    cin>>a;
-    for(i=0;i<a;i++)
-        {
-        cin>>m>>n;
-        cout<<comb(n,m)<<endl;
-    }
-    return 0;
-}
+//works in python -- all test cases successful
+def ans(n,m):
+    k = 1
+    p = 1000000007
+    for i in range (m+1,n+m+1):
+        k*=i
+        k/=(i-m)
+    return k%p
+t = input()
+for j in range(t):
+    n,m = raw_input().split()
+    print ans(int(n),int(m))
